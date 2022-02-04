8a6df8a17d10664aca41cba0d3f8ea9241efe62e
acc18jh <jhale2@sheffield.ac.uk>
2022-02-04 16:25:46 +0000

    Test onSelect function in app.js

--- app.js
+++ app.js
@@ -51,8 +51,10 @@ class App{
 	const self = this;
 	let controller;
 	    
+	// When screen is clicked?
 	function onSelect() {
-		const material = new THREE.MeshPhongMaterial( { color: 0xFFFFFF * Math.random() });
+		//const material = new THREE.MeshPhongMaterial( { color: 0xFFFFFF * Math.random() });
+		const material = new THREE.MeshPhongMaterial( { color: 0xFFFFFF });
 		const mesh = new THREE.Mesh( self.geometry, material );
 		mesh.position.set( 0,0,-0.3).applyMatrix4( controller.matrixWorld );
 		mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
