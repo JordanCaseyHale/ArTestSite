3ec3c2ede8663073f2c753ce8a1d759ada4dd058
acc18jh <jhale2@sheffield.ac.uk>
2022-02-06 13:18:55 +0000

    Test Line

--- app.js
+++ app.js
@@ -41,7 +41,21 @@ class App{
 	}	
     
     initScene(){
-        this.geometry = new THREE.BoxBufferGeometry( 0.06, 0.06, 0.06 ); 
+    	const material = new THREE.LineBasicMaterial({
+			color: 0x0000ff
+		});
+
+		const points = [];
+		points.push( new THREE.Vector3( - 10, 0, 0 ) );
+		points.push( new THREE.Vector3( 0, 10, 0 ) );
+		points.push( new THREE.Vector3( 10, 0, 0 ) );
+
+		const geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+		const line = new THREE.Line( geometry, material );
+		this.scene.add( line );
+
+        this.geometry = new THREE.BoxBufferGeometry( 0.06, 0.06, 0.06 );
         this.meshes = [];
     }
     
