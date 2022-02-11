783024e952db07d91f2f214c03fb4047ebcee370
acc18jh <jhale2@sheffield.ac.uk>
2022-02-11 16:03:15 +0000

    1.8.5 Visible test

--- graph.js
+++ graph.js
@@ -1,6 +1,6 @@
 AFRAME.registerComponent('graph', {
     init: function () {
         console.log('build graph');
-        <a-box position="0 3 0" rotation="0 45 0" color="#FFFFFF"></a-box>
+        this.el.visible = true;
     }
 });
