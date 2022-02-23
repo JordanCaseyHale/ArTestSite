452263818df2155d9484d401db22bf964ca9cc1d
Jordan Hale <jordancaseyhale@gmail.com>
2022-02-23 18:54:55 +0000

    2.5 Debug dataHandler.js

--- dataHandler.js
+++ dataHandler.js
@@ -7,27 +7,33 @@ function time_string_to_normalised_points(id) {
 	// Get the data points into time values
 	const timeValues = [];
 	for (var i=0; i<(dataPoints.length); i++) {
+		console.log(i, dataPoints[i]);
 		var date = new Date(dataPoints[i]);
+		console.log('Date:', date);
 
 		var hours = date.getHours();
 		var mins = date.getMinutes();
 
 		timeValues[i] = (hours * 60) + mins
+		console.log('timeValue:', timeValues[i]);
 	}
 
 	// Select Max
 	var maxValue = Math.max(timeValues);
 	var maxIndex = timeValues.indexOf(maxValue);
+	console.log(maxValue, maxIndex);
 
 	// Select Min
 	var minValue = Math.min(timeValues);
 	var minIndex = timeValues.indexOf(minValue);
+	console.log(minValue, minIndex);
 
 	// points normalised
 	var normalisedPoints = [];
 	for (var i=0; i<timeValues.length; i++) {
 		normalisedPoints[i] = (timeValues[i]-minValue)/(maxValue-minValue);
 	}
+	console.log('normalisedPoints',normalisedPoints);
 
 	return minIndex, maxIndex, normalisedPoints;
 }
