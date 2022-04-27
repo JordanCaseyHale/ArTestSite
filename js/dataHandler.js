2a1eebaf2bb94fc8335dd5e2ed92ab19982f1b1b
Jordan Hale <jordancaseyhale@gmail.com>
2022-04-27 16:07:00 +0100

    Invert data range

--- js/dataHandler.js
+++ js/dataHandler.js
@@ -57,9 +57,12 @@ function time_string_to_normalised_points_given_max_min(dataPoints, max, min) {
 	var minValue = (minHours * 60) + minMins;
 
 	// points normalised
+	var zeroToOne = 0;
 	var normalisedPoints = [];
 	for (var i=0; i<timeValues.length; i++) {
-		normalisedPoints[i] = ((timeValues[i]-minValue)/(maxValue-minValue) * 2) -1;
+		zeroToOne = (timeValues[i]-minValue)/(maxValue-minValue);
+		// Invert between zero and one, increase range, then shift range
+		normalisedPoints[i] = ((1-zeroToOne) * 2) -1;
 	}
 
 	return normalisedPoints;
@@ -75,9 +78,12 @@ function numbers_to_normalised_points(dataPoints) {
 	var minIndex = dataPoints.indexOf(minValue);
 
 	// normalise points
+	var zeroToOne = 0;
 	var normalisedPoints = [];
 	for (var i=0; i<dataPoints.length; i++) {
-		normalisedPoints[i] = ((dataPoints[i]-minValue)/(maxValue-minValue) * 2) - 1;
+		zeroToOne = (dataPoints[i]-minValue)/(maxValue-minValue);
+		// Invert between zero and one, increase range, then shift range
+		normalisedPoints[i] = ((1-zeroToOne) * 2) - 1;
 	}
 
 	return normalisedPoints;
